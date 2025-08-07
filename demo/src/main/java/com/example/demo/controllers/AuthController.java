824a135a06a5925bc6303c5f6f10dc1f6c3cda6a
Nhan121631 <phamphunhanpham8@gmail.com>
2025-08-07 11:13:53 +0700

    Add login and test endpoints to AuthController and create Docker README

--- demo/src/main/java/com/example/demo/controllers/AuthController.java
+++ demo/src/main/java/com/example/demo/controllers/AuthController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +24,10 @@ public class AuthController {
     public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto request) throws Exception {
         LoginResponseDto result = this.userService.login(request);
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Test successful");
     }
 }
